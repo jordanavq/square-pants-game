81e8e4eafa3ac1b3097e9c9a84c4902197c05a9b
jordanavq <jordanavq@gmail.com>
2021-06-24 16:49:51 -0300

    class SpongeBob + spongeBob moviments

---
+++ js/game.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas .getContext('2d');
+
+const cWidth = canvas.width;
+const cHeight = canvas.height;
+
+
+/* 
+function clearCanvas() {
+    ctx.clearRect (0,0,cWidth,cHeight);
+};
+
+function
+
+
+const draw = (x,y,w,h,color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect (x,y,w,h);
+}
+
+draw(20, 40, 60, 100, 'orange'); */
+
+
+ class SpongeBob {
+    constructor () {
+        this.x = 25; //posição x do Bob Esponja
+        this.y = 25; //posição y do Bob Esponja
+        const img = new Image();
+        img.addEventListener ('load', () => {
+            this.img = img;
+            this.draw();
+        });
+
+        img.src ="/images/spongebob.png";
+
+    }
+
+    draw () {
+        ctx.drawImage (this.img, this.x, this.y, 80, 100);
+    }
+
+    //Sponge Bob moviments
+    moveUp () { 
+        this.y -= 25;
+    }
+
+    moveDown () {
+        this.y += 25;
+    }
+
+    moveRight () {
+        this.x +=25;
+    }
+
+    moveLeft () {
+        this.x -= 25;
+    }
+ 
+}
+
+const spongeBob = new SpongeBob ();
+
+function updateCanvas() {
+    ctx.clearRect (0,0,cWidth,cHeight);
+    spongeBob.draw();
+    requestAnimationFrame (updateCanvas);
+}
