c3b5ef1965845483520aeff06336caecb22b7b26
jordanavq <jordanavq@gmail.com>
2021-06-24 16:51:15 -0300

    class SpongeBob + spongeBob moviments

--- js/game.js
+++ js/game.js
@@ -1,9 +1,8 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas .getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 const cWidth = canvas.width;
 const cHeight = canvas.height;
-
 
 /* 
 function clearCanvas() {
@@ -20,48 +19,67 @@ const draw = (x,y,w,h,color) => {
 
 draw(20, 40, 60, 100, 'orange'); */
 
+class SpongeBob {
+  constructor() {
+    this.x = 25; //posição x do Bob Esponja
+    this.y = 25; //posição y do Bob Esponja
+    const img = new Image();
+    img.addEventListener("load", () => {
+      this.img = img;
+      this.draw();
+    });
 
- class SpongeBob {
-    constructor () {
-        this.x = 25; //posição x do Bob Esponja
-        this.y = 25; //posição y do Bob Esponja
-        const img = new Image();
-        img.addEventListener ('load', () => {
-            this.img = img;
-            this.draw();
-        });
+    img.src = "/images/spongebob.png";
+  }
 
-        img.src ="/images/spongebob.png";
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, 80, 100);
+  }
 
-    }
+  //Sponge Bob moviments
+  moveUp() {
+    this.y -= 25;
+  }
 
-    draw () {
-        ctx.drawImage (this.img, this.x, this.y, 80, 100);
-    }
+  moveDown() {
+    this.y += 25;
+  }
 
-    //Sponge Bob moviments
-    moveUp () { 
-        this.y -= 25;
-    }
+  moveRight() {
+    this.x += 25;
+  }
 
-    moveDown () {
-        this.y += 25;
-    }
-
-    moveRight () {
-        this.x +=25;
-    }
-
-    moveLeft () {
-        this.x -= 25;
-    }
- 
+  moveLeft() {
+    this.x -= 25;
+  }
 }
 
-const spongeBob = new SpongeBob ();
+const spongeBob = new SpongeBob();
 
 function updateCanvas() {
-    ctx.clearRect (0,0,cWidth,cHeight);
-    spongeBob.draw();
-    requestAnimationFrame (updateCanvas);
+  ctx.clearRect(0, 0, cWidth, cHeight);
+  spongeBob.draw();
+  requestAnimationFrame(updateCanvas);
 }
+
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 38:
+      spongeBob.moveUp();
+      console.log("up", spongeBob);
+      break;
+    case 40:
+      spongeBob.moveDown();
+      console.log("down", spongeBob);
+      break;
+    case 37:
+      spongeBob.moveLeft();
+      console.log("left", spongeBob);
+      break;
+    case 39:
+      spongeBob.moveRight();
+      console.log("right", spongeBob);
+      break;
+  }
+  updateCanvas();
+});
