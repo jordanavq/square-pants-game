d3fc219cad009172f4e785635bfa9ab227bba990
jordanavq <jordanavq@gmail.com>
2021-06-25 14:50:43 -0300

    add sponge bob

--- js/game2.js
+++ js/game2.js
@@ -5,6 +5,7 @@ window.onload = () => {
 
   function startGame() {
     background.draw();
+    player.draw();
   }
 
   const canvas = document.getElementById("canvas");
@@ -27,4 +28,23 @@ window.onload = () => {
     }
   }
   const background = new Background("./images/backgroundImage.png");
+
+  class SpongeBob {
+    constructor(source, x, y, w, h) {
+      this.x = x;
+      this.y = y;
+      this.width = w;
+      this.height = h;
+
+      const img = new Image();
+      img.src = source;
+      img.onload = () => {
+        this.img = img;
+      };
+    }
+    draw() {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+  }
+  const player = new SpongeBob("../images/spongebob8.png", 25, 200, 80, 100);
 };
