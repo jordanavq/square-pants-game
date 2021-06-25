43e0cb646c59ea53a4dbb38e088dfbc07084fcd5
jordanavq <jordanavq@gmail.com>
2021-06-25 18:13:01 -0300

    add keycode directions for Sponge Bob

--- js/game2.js
+++ js/game2.js
@@ -1,15 +1,52 @@
+//Botão start
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {
-    background.draw();
-    player.draw();
-  }
+  //botões para movimentar o
+  document.addEventListener("keydown", (e) => {
+    //console.log(e) P/ ver o keycode
 
+    switch (e.key) {
+      case "ArrowLeft":
+        player.moveLeft();
+        break;
+
+      case "ArrowRight":
+        player.moveRight();
+        break;
+
+      case "ArrowUp":
+        player.moveUp();
+        break;
+
+      case "ArrowDown":
+        player.moveDown();
+        break;
+    }
+  });
+
+  function startGame() {
+    updateCanvas();
+  }
+  //Canvas
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
+
+  let animationId = null;
+
+  //Função updateCanvas=>limpa o canvas e desenha
+  function updateCanvas() {
+    clearCanvas();
+    background.draw();
+    player.draw();
+    animationId = requestAnimationFrame(updateCanvas);
+  }
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
   class Background {
     constructor(source) {
@@ -35,6 +72,7 @@ window.onload = () => {
       this.y = y;
       this.width = w;
       this.height = h;
+      this.speed = 10;
 
       const img = new Image();
       img.src = source;
@@ -45,6 +83,19 @@ window.onload = () => {
     draw() {
       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
+    moveLeft() {
+      this.x -= this.speed;
+    }
+    moveRight() {
+      this.x += this.speed;
+    }
+    moveUp() {
+      this.y -= this.speed;
+    }
+    moveDown() {
+      this.y += this.speed;
+    }
   }
+
   const player = new SpongeBob("../images/spongebob8.png", 25, 200, 80, 100);
 };
