bbf194ae183f7a20bf0999f999622a32170ab8ff
jordanavq <jordanavq@gmail.com>
2021-06-26 01:17:52 -0300

    background moving effect

--- js/game2.js
+++ js/game2.js
@@ -40,6 +40,7 @@ window.onload = () => {
   function updateCanvas() {
     clearCanvas();
     background.draw();
+    background.move();
     player.draw();
     animationId = requestAnimationFrame(updateCanvas);
   }
@@ -52,6 +53,7 @@ window.onload = () => {
     constructor(source) {
       this.x = 0;
       this.y = 0;
+      this.speed = -1;
 
       const img = new Image();
       img.src = source;
@@ -62,6 +64,15 @@ window.onload = () => {
 
     draw() {
       ctx.drawImage(this.img, this.x, this.y, canvas.width, canvas.height);
+      if (this.speed < 0) {
+        ctx.drawImage(this.img, this.x + canvas.width, 0);
+      } else {
+        ctx.drawImage(this.img, this.x - this.img.width, 0);
+      }
+    }
+    move() {
+      this.x += this.speed;
+      this.x %= canvas.width;
     }
   }
   const background = new Background("./images/backgroundImage.png");
@@ -105,5 +116,38 @@ window.onload = () => {
     }
   }
 
-  const player = new SpongeBob("../images/spongebob8.png", 25, 200, 80, 100);
+  const player = new SpongeBob("../images/spongebob1.png", 25, 200, 80, 100);
+
+  /* class Obstacle {
+    constructor (source, y) {
+      
+      this.size = 40;
+      this.x = this.canvas.width + this.size/2;
+      this.y = y;
+      this.speed = 3;
+      this.direction = -2;
+      
+
+      const img = new Image();
+      img.src = source;
+      img.onload = () => {
+        this.img = img;
+      };
+
+      draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.size, this.size);
+    }
+      move() {
+        this.x += this.direction * this.speed;
+        
+      }
+    } 
+    const obstacles = [];
+    function createObstacle{
+      
+      this.size = 40;
+      this.x = Math.random()this.canvas.width + this.size/2;
+      this.y = y;
+
+    } */
 };
